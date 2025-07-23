73f5b1931ddef1d09f79e28a6c34032fffd67029
ibras0696 <ibras0696>
2025-07-24 00:29:58 +0300

    исправление бага

--- gifts_system/func_push_bot.py
+++ gifts_system/func_push_bot.py
@@ -10,11 +10,12 @@ from database import init_db
 
 
 async def push_bot_group_message(txt: str):
-    # Отправка сообщения в группу
-    group_id = GRPS[0]
     # Телеграм бот
     bot_tg = Bot(token=TOKEN_BOT)
     # Получение списка администраторов группы
-    await bot_tg.send_message(chat_id=group_id, text=txt)
+    try:
+        await bot_tg.send_message(chat_id=GRPS[0], text=txt)
+    finally:
+        await bot_tg.close()
 
 
