ffa51d953f99128e09fab4d22b826839a9fd91a9
ibras0696 <ibras0696>
2025-07-24 00:26:25 +0300

    токен бота

--- gifts_system/func_push_bot.py
+++ gifts_system/func_push_bot.py
@@ -4,7 +4,7 @@ import logging
 from aiogram import Dispatcher, Bot
 from aiogram.exceptions import TelegramBadRequest
 
-from config import BOT_TOKEN, GRPS
+from config import TOKEN_BOT, GRPS
 
 from database import init_db
 
@@ -13,7 +13,7 @@ async def push_bot_group_message(txt: str):
     # Отправка сообщения в группу
     group_id = GRPS[0]
     # Телеграм бот
-    bot_tg = Bot(token=BOT_TOKEN)
+    bot_tg = Bot(token=TOKEN_BOT)
     # Получение списка администраторов группы
     await bot_tg.send_message(chat_id=group_id, text=txt)
 
